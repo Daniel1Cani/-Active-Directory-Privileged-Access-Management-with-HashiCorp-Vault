1b4879e862c6cd61a5161d17b620ec5242486c65
Daniel1Cani <cani.daniel03@gmail.com>
2025-10-26 20:24:36 -0400

    Update Vault-start.ps1

--- Vault-start.ps1
+++ Vault-start.ps1
@@ -1,2 +1,3 @@
-# Start Vault manually
+# Starts Vault server with specified config file.
+# Run as Administrator in PowerShell.
 Start-Process -NoNewWindow -FilePath "C:\vault\vault.exe" -ArgumentList 'server -config="C:\vault\config.hcl"'
